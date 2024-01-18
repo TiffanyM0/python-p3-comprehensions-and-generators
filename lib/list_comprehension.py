4759bc1801a349084babaae4b07b981e7b6f5529
TiffanyMaina <tiff_shiro@DESKTOP-Q44J8P0.>
2024-01-18 23:39:26 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,22 @@
 
 def return_evens(num_list):
     pass
+    
+    even_numbers = [n for n in num_list if n%2 == 0]
+    return even_numbers
+    # for n in num_list:
+    #     even_numbers = list()
+    #     if n%2 == 0:
+    #         even_numbers.append(n)
+    #     else:
+    #         return even_numbers
 
 def make_exclamation(sentence_list):
     pass
+    exclaim_word = [(n + "!") for n in sentence_list]
+    # if len(sentence_list) == 0:
+    #     return []
+    # elif len(sentence_list) > 0:
+    #     for n in sentence_list:
+    #         exclaim_word = n + "!"
+    return exclaim_word
